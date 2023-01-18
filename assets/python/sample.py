41a6eaa3eb7cf3bfc81b952c4c7a080d15048564
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2023-01-18 14:51:20 +0000

    Built site for gh-pages

--- assets/python/sample.py
+++ assets/python/sample.py
@@ -4,8 +4,8 @@ import pandas as pd
 from pathlib import Path
 
 
-def find_and_load_files(file_path: Union[str, Path], file_type: str):
-    """Recursively find and load files of the stated type along the given file path."""
+def find_and_load_files(file_path: Union[str, Path], file_type: str) -> list:
+    """Recursively find files of the stated type along the given file path."""
     # We have a really long comment on this line just for demonstration purposes so that we can generate a few errors that need linting
     try:
         return Path(file_path).rglob(f"*{file_type}")
